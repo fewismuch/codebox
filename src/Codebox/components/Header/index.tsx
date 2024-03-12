db395acfa2828b8af2d6db3cee64a3bde82efa25
tianyu <779205344@qq.com>
2024-03-12 23:40:14 +0800

    fix: 修复代码粘贴问题

--- src/Codebox/components/Header/index.tsx
+++ src/Codebox/components/Header/index.tsx
@@ -16,7 +16,7 @@ export const Header: React.FC<IHeader> = () => {
   const [copyed, setCopyed] = useState(false)
 
   const filesCode = useMemo(() => {
-    return `window.codeboxFiles = ${JSON.stringify(sandpack.files, null, 2).replace(/<\/script>/g, '</script>')}`
+    return `window.codeboxFiles = ${JSON.stringify(sandpack.files, null, 2).replace(/<\/script>/g, '<\\/script>')}`
   }, [sandpack.files])
 
   const copyCode = () => {
